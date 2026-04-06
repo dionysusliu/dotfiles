6669a23fa1931e7d6e4edccce840db3ba321918d
chuang <chuangliu0815@gmail.com>
2026-04-05 20:32:29 -0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+set -ex 
 
 # sync dotfile setups to current dir
 DOTFILES="$HOME/dotfiles"
 
-cp ~/.config/zellij/config.kdl "$DOTFILES/zellij/config.kdl"
+# cli
+## nvim
+rsync -a --exclude='.DS_Store' ~/.config/nvim/ "$DOTFILES/cli/nvim/"
+## zellij
+rsync -a ~/.config/zellij/config.kdl "$DOTFILES/cli/zellij/config.kdl"
+## yazi
+rsync -a --exclude='plugins/' --exclude='.DS_Store' ~/.config/yazi/ "$DOTFILES/cli/yazi/"
+## lazygit
+rsync -a ~/Library/Application\ Support/lazygit/config.yml "$DOTFILES/cli/lazygit/config.yml"
+
+# system
+## karabiner
+rsync -a ~/.config/karabiner/karabiner.json "$DOTFILES/system/karabiner/karabiner.json"
+## raycast
+rsync -a --exclude='.DS_Store' ~/.config/raycast/ "$DOTFILES/system/raycast/"
+
+
+# brew
+brew bundle dump --file "$DOTFILES/Brewfile" --force
 
 echo "✓ synced configs to dotfiles"
